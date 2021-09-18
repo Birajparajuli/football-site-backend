7e91aaf1d6e2ca317feacec7ba946170eb665842
Biraj <birajparajuli2001@gmail.com>
2021-09-18 13:09:54 +0545

    google auth added

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://football-site-backend.herokuapp.com/",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9bfca35eb8527df0e11fa4e5ae6ed249'),
+      secret: env("ADMIN_JWT_SECRET", "9bfca35eb8527df0e11fa4e5ae6ed249"),
     },
   },
 });
